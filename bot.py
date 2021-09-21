1f4cda4b7de9d300729a99e5d661cf3b1ebf6e1b
Aristocrab <maksymkanupa@gmail.com>
2021-09-21 11:50:50 +0300

    Added DB

--- bot.py
+++ bot.py
@@ -1,51 +1,107 @@
+from telegram.ext import Updater, CommandHandler
+import sqlite3
 import logic
-import telebot
 
-bot = telebot.TeleBot("1840339382:AAEJmiBJh457AtDKMeuhk4QgALGv6iMgtdo", parse_mode="MARKDOWN")
-
-
-@bot.message_handler(commands=['today'])
-def today(message):
-    bot.send_message(message.chat.id, logic.get_lessons_for_today(708))
+updater = Updater(token="1840339382:AAEJmiBJh457AtDKMeuhk4QgALGv6iMgtdo", use_context=True)
+dispatcher = updater.dispatcher
 
 
-@bot.message_handler(commands=['tomorrow'])
-def tomorrow(message):
-    bot.send_message(message.chat.id, logic.get_lessons_for_tomorrow(708))
+def message_handler(command):
+    def decorator(function):
+        def wrapper():
+            global dispatcher
+            handler = CommandHandler(command, function)
+            dispatcher.add_handler(handler)
+
+        wrapper()
+
+    return decorator
 
 
-@bot.message_handler(commands=['week'])
-def week(message):
-    bot.send_message(message.chat.id, logic.get_lessons_for_this_week(708))
+@message_handler(command="r")
+def r(update, context):
+    if update.message.text.startswith("/r "):
+        group = logic.get_group_id(update.message.text)
+        if group != -1:
+            context.bot.send_message(update.effective_chat.id, f"Групу змінено на *{update.message.text[3:]}*",
+                                     parse_mode='Markdown')
+
+            cursor.execute(f'INSERT or REPLACE INTO groups VALUES ({update.effective_chat.id}, {group});')
+            sqlite_connection.commit()
+        else:
+            context.bot.send_message(update.effective_chat.id, "Групу не знайдено", parse_mode='Markdown')
+    else:
+        context.bot.send_message(update.effective_chat.id, "Виберіть групу: /r бс-14", parse_mode='Markdown')
 
 
-@bot.message_handler(commands=['nextweek'])
-def nextweek(message):
-    bot.send_message(message.chat.id, logic.get_lessons_for_next_week(708))
+@message_handler(command="today")
+def today(update, context):
+    cursor.execute(f'SELECT group_id FROM groups WHERE user_id = {update.effective_chat.id};')
+    group = cursor.fetchone()[0]
+
+    context.bot.send_message(update.effective_chat.id, logic.get_lessons_for_today(group),
+                             parse_mode='Markdown')
 
 
-@bot.message_handler(commands=['full'])
-def full(message):
-    this_week = logic.get_lessons_for_this_week(708, full = True)
-    next_week = logic.get_lessons_for_next_week(708, full = True)
-    bot.send_message(message.chat.id, this_week)
-    bot.send_message(message.chat.id, next_week)
+@message_handler(command="tomorrow")
+def tomorrow(update, context):
+    cursor.execute(f'SELECT group_id FROM groups WHERE user_id = {update.effective_chat.id};')
+    group = cursor.fetchone()[0]
+
+    context.bot.send_message(update.effective_chat.id, logic.get_lessons_for_tomorrow(group),
+                             parse_mode='Markdown')
 
 
-@bot.message_handler(commands=['who'])
-def who(message):
-    bot.send_message(message.chat.id, logic.get_teachers_name(708))
+@message_handler(command="week")
+def week(update, context):
+    cursor.execute(f'SELECT group_id FROM groups WHERE user_id = {update.effective_chat.id};')
+    group = cursor.fetchone()[0]
+
+    context.bot.send_message(update.effective_chat.id, logic.get_lessons_for_this_week(group),
+                             parse_mode='Markdown')
 
 
-@bot.message_handler(commands=['timetable'])
-def timetable(message):
-    bot.send_message(message.chat.id, "_1 пара_  08-30 - 10-05\n" 
-        + "_2 пара_  10-25 - 12-00\n" 
-        + "_3 пара_  12-20 - 13-55\n" 
-        + "_4 пара_  14-15 - 15-50\n" 
-        + "_5 пара_  16-10 - 17-45")
+@message_handler(command="nextweek")
+def nextweek(update, context):
+    cursor.execute(f'SELECT group_id FROM groups WHERE user_id = {update.effective_chat.id};')
+    group = cursor.fetchone()[0]
+
+    context.bot.send_message(update.effective_chat.id, logic.get_lessons_for_next_week(group),
+                             parse_mode='Markdown')
+
+
+@message_handler(command="full")
+def full(update, context):
+    cursor.execute(f'SELECT group_id FROM groups WHERE user_id = {update.effective_chat.id};')
+    group = cursor.fetchone()[0]
+
+    this_week = logic.get_lessons_for_this_week(group, full=True)
+    next_week = logic.get_lessons_for_next_week(group, full=True)
+
+    context.bot.send_message(update.effective_chat.id, this_week, parse_mode='Markdown')
+    context.bot.send_message(update.effective_chat.id, next_week, parse_mode='Markdown')
+
+
+@message_handler(command="who")
+def who(update, context):
+    cursor.execute(f'SELECT group_id FROM groups WHERE user_id = {update.effective_chat.id};')
+    group = cursor.fetchone()[0]
+
+    context.bot.send_message(update.effective_chat.id, logic.get_teachers_name(group), parse_mode='Markdown')
+
+
+@message_handler(command="timetable")
+def timetable(update, context):
+    context.bot.send_message(update.effective_chat.id, "_1 пара_  08-30 - 10-05\n"
+                             + "_2 пара_  10-25 - 12-00\n"
+                             + "_3 пара_  12-20 - 13-55\n"
+                             + "_4 пара_  14-15 - 15-50\n"
+                             + "_5 пара_  16-10 - 17-45", parse_mode='Markdown')
 
 
 if __name__ == "__main__":
     print("Bot started")
-    bot.polling()
+    sqlite_connection = sqlite3.connect('user_groups.db', check_same_thread=False)
+    cursor = sqlite_connection.cursor()
+    cursor.execute('CREATE TABLE if not exists groups (user_id INTEGER PRIMARY KEY, group_id INTEGER);')
+    updater.start_polling()
--- logic.py
+++ logic.py
@@ -1,69 +1,149 @@
-from datetime import datetime, time
+import datetime
+import urllib.parse
+
 import requests
 
+
+def get_group_id(text):
+    url = "https://api.rozklad.org.ua/v2/groups/?search={%27query%27:%27" + urllib.parse.quote(text[3:]) + "%27}"
+    request = requests.get(url)
+
+    if request.status_code != 200:
+        return -1
+
+    return request.json()["data"][0]["group_id"]
+
+
 def get_lessons_for_today(group_id):
-    return get_lessons_for_day(group_id, day_number = (datetime.today().weekday() + 1), day = "Сьогодні")
+    return get_lessons_for_day(group_id, day_number=(datetime.datetime.today().weekday() + 1), day="Сьогодні")
+
+
 def get_lessons_for_tomorrow(group_id):
-    if(datetime.today().weekday() + 1 == 7):
-        return get_lessons_for_day(group_id, day_number = ((datetime.today().weekday() + 1) + 1) % 7, day = "Завтра", lesson_week = ["2", "1"][requests.get("https://api.rozklad.org.ua/v2/weeks").json()["data"] - 1])
-    return get_lessons_for_day(group_id, day_number = ((datetime.today().weekday() + 1) + 1) % 7, day = "Завтра")
+    if datetime.datetime.today().weekday() + 1 == 7:
+        return get_lessons_for_day(group_id, day_number=((datetime.datetime.today().weekday() + 1) + 1) % 7,
+                                   day="Завтра", lesson_week=["2", "1"][
+                requests.get("https://api.rozklad.org.ua/v2/weeks").json()["data"] - 1])
+
+    return get_lessons_for_day(group_id, day_number=((datetime.datetime.today().weekday() + 1) + 1) % 7, day="Завтра")
 
 
-def get_lessons_for_day(group_id, day_number, day, lesson_week = str(requests.get("https://api.rozklad.org.ua/v2/weeks").json()["data"])):
+def get_lessons_for_day(group_id, day_number, day,
+                        lesson_week=str(requests.get("https://api.rozklad.org.ua/v2/weeks").json()["data"])):
+    if group_id is None:
+        return "Виберіть групу: /r бс-14"
+
+    print(group_id)
+
     url = "https://api.rozklad.org.ua/v2/groups/"
-    request_url = url + str(group_id) + "/lessons?filter={'day_number':" + str(day_number) + ",'lesson_week':" + lesson_week + "}"
+
+    request_url = url + str(group_id) + "/lessons?filter={'day_number':" + str(
+        day_number) + ",'lesson_week':" + lesson_week + "}"
+    print(request_url)
     request = requests.get(request_url)
-    if(request.status_code != 200):
+    if request.status_code != 200:
         return f"{day} пар немає"
     response = request.json()["data"]
     lessons = ""
     lessons += f'*{response[0]["day_name"]}* ({lesson_week})\n'
 
     for i in range(len(response)):
-        lessons += response[i]["lesson_number"] + ") " + response[i]["lesson_name"] + " `" + response[i]["lesson_type"] + " " + response[i]["rooms"][0]["room_name"] + "`\n"
+        if len(response[i]["rooms"]) != 0:
+            lessons += response[i]["lesson_number"] + ") " + response[i]["lesson_name"] + " `" + response[i][
+                "lesson_type"] + " " + response[i]["rooms"][0]["room_name"] + "`\n"
+        else:
+            lessons += response[i]["lesson_number"] + ") " + response[i]["lesson_name"] + " `" + response[i][
+                "lesson_type"] + "`\n"
 
     return lessons
 
 
-def get_teachers_name(group_id):
+def get_lessons_for_this_week(group_id, full=False):
+    return get_lessons_for_week(group_id,
+                                lesson_week=requests.get("https://api.rozklad.org.ua/v2/weeks").json()["data"],
+                                full=full)
+
+
+def get_lessons_for_next_week(group_id, full=False):
+    return get_lessons_for_week(group_id, lesson_week=(requests.get("https://api.rozklad.org.ua/v2/weeks")
+                                                       .json()["data"] + 1) % 2, full=full)
+
+
+def get_lessons_for_week(group_id, lesson_week, full=False):
+    if group_id is None:
+        return "Виберіть групу: /r бс-14"
+
     url = "https://api.rozklad.org.ua/v2/groups/"
-    request_url = url + str(group_id) + "/lessons?filter={'day_number':" + str(datetime.today().weekday() + 1) + ",'lesson_week':" + str(requests.get("https://api.rozklad.org.ua/v2/weeks").json()["data"]) + "}"
+    request_url = url + str(group_id) + "/lessons?filter={'lesson_week':" + str(lesson_week) + "}"
     request = requests.get(request_url)
-    if(request.status_code != 200):
+    response = request.json()["data"]
+    day_name = ""
+    lessons = ""
+
+    for i in range(len(response)):
+        if day_name != response[i]["day_name"]:
+            day_name = response[i]["day_name"]
+            if i > 0:
+                lessons += "\n"
+            lessons += f'*{day_name}*\n'
+
+        if len(response[i]["rooms"]) != 0:
+            lessons += response[i]["lesson_number"] + ") " + response[i]["lesson_name"] + " `" + response[i][
+                "lesson_type"] + " " + response[i]["rooms"][0]["room_name"] + "`\n"
+        else:
+            lessons += response[i]["lesson_number"] + ") " + response[i]["lesson_name"] + " `" + response[i][
+                "lesson_type"] + "`\n"
+        if full:
+            lessons += "     " + response[i]["teacher_name"] + "\n"
+
+    lessons = "*" + ["Перша", "Друга"][lesson_week - 1] + " неділя*\n" + lessons
+    return lessons
+
+
+def get_teachers_name(group_id):
+    if group_id is None:
+        return "Виберіть групу: /r бс-14"
+
+    url = "https://api.rozklad.org.ua/v2/groups/"
+    request_url = url + str(group_id) + "/lessons?filter={'day_number':" + str(
+        datetime.datetime.today().weekday() + 1) + ",'lesson_week':" + str(
+        requests.get("https://api.rozklad.org.ua/v2/weeks").json()["data"]) + "}"
+    request = requests.get(request_url)
+    if request.status_code != 200:
         return f"Сьогодні пар немає"
     response = request.json()["data"]
-    
-    now = datetime.now().time()
+
+    now = datetime.datetime.now().time()
     current_lesson = get_lesson(now)
     current_break = get_break(now)
     name = ""
-    
-    if(current_lesson != -1):
+
+    if current_lesson != -1:
         for teacher in response[current_lesson]["teachers"]:
-            name += teacher["teacher_full_name"] + "\n"
-    elif(current_break != -1):
-        name += f"Попередня ({current_break + 1}-а) пара:"
+            name += teacher["teacher_name"].replace("посада ", "").replace("викладач ", "") + "\n"
+    elif current_break != -1:
+        name += f"*Попередня ({current_break + 1}-а) пара:*"
         for teacher in response[current_break]["teachers"]:
-            name += teacher["teacher_full_name"] + "\n"
-        
+            name += teacher["teacher_name"].replace("посада ", "").replace("викладач ", "") + "\n"
+
         name += "\n"
 
-        name += f"Наступна ({current_break + 1}-а) пара:"
-        for teacher in response[current_break+1]["teachers"]:
-            name += teacher["teacher_full_name"] + "\n"
+        name += f"*Наступна ({current_break + 1}-а) пара:*"
+        for teacher in response[current_break + 1]["teachers"]:
+            name += teacher["teacher_name"].replace("посада ", "").replace("викладач ", "") + "\n"
 
     return name
 
 
+# Utils
 def get_lesson(current_time):
     lessons = [
-        [time(8, 30), time(10, 5)],
-        [time(10, 25), time(12, 0)],
-        [time(12, 20), time(13, 55)],
-        [time(14, 15), time(15, 50)],
-        [time(16, 10), time(17, 45)],
+        [datetime.time(8, 30), datetime.time(10, 5)],
+        [datetime.time(10, 25), datetime.time(12, 0)],
+        [datetime.time(12, 20), datetime.time(13, 55)],
+        [datetime.time(14, 15), datetime.time(15, 50)],
+        [datetime.time(16, 10), datetime.time(17, 45)],
     ]
-    if time(8, 0) <= current_time <= lessons[0][1]:
+    if datetime.time(8, 0) <= current_time <= lessons[0][1]:
         return 0
     elif lessons[1][0] <= current_time <= lessons[1][1]:
         return 1
@@ -78,10 +158,10 @@ def get_lesson(current_time):
 
 def get_break(current_time):
     breaks = [
-        [time(10, 5), time(10, 25)],
-        [time(12, 0), time(12, 20)],
-        [time(13, 55), time(14, 15)],
-        [time(15, 50), time(16, 10)],
+        [datetime.time(10, 5), datetime.time(10, 25)],
+        [datetime.time(12, 0), datetime.time(12, 20)],
+        [datetime.time(13, 55), datetime.time(14, 15)],
+        [datetime.time(15, 50), datetime.time(16, 10)],
     ]
     if breaks[0][0] < current_time < breaks[0][1]:
         return 0
@@ -92,32 +172,3 @@ def get_break(current_time):
     elif breaks[3][0] <= current_time <= breaks[3][1]:
         return 3
     return -1
-
-
-def get_lessons_for_this_week(group_id, full = False):
-    return get_lessons_for_week(group_id, lesson_week = 2 - datetime.today().isocalendar().week % 2, full = full)
-def get_lessons_for_next_week(group_id, full = False):
-    return get_lessons_for_week(group_id, lesson_week = 2 - abs(datetime.today().isocalendar().week % 2 - 1), full = full)
-
-
-def get_lessons_for_week(group_id, lesson_week, full = False):
-    url = "https://api.rozklad.org.ua/v2/groups/"
-    request_url = url + str(group_id) + "/lessons?filter={'lesson_week':" + str(lesson_week) + "}"
-    request = requests.get(request_url)
-    response = request.json()["data"]
-    day_name = ""
-    lessons = ""
-
-    for i in range(len(response)):
-        if(day_name != response[i]["day_name"]):
-            day_name = response[i]["day_name"]
-            if(i > 0):
-                lessons += "\n"
-            lessons += f'*{day_name}*\n'
-
-        lessons += response[i]["lesson_number"] + ") " + response[i]["lesson_name"] + " `" + response[i]["lesson_type"] + " " + response[i]["rooms"][0]["room_name"] + "`\n"
-        if(full):
-            lessons += "     " + response[i]["teacher_name"] + "\n"
-
-    lessons = "*" + ["Перша", "Друга"][lesson_week - 1] + " неділя*\n" + lessons
-    return lessons
